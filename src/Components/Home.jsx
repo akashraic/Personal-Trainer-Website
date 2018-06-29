b11ad2f1b1de3056bf3b0cf5ecb976e90c004357
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-06-29 19:32:54 -0400

    added logo to home.jsx and applying css styles

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,8 +7,17 @@ class Home extends Component {
     render() {
         return (
             <Grid>
-                <h1>Holistic</h1>
-
+                <Row className="show-grid tet-center">
+                    <Col xs={4}>
+                        <h1>Holistic</h1>
+                    </Col>
+                    <Col className="Logo-wrapper" xs={6}>
+                        <Image src="assests/Main_Logo.png" circle className="logo"/>
+                    </Col>
+                    <Col xs={2}>
+                        <h1>Bodies</h1>
+                    </Col>
+                </Row>
             </Grid>
         );
     }
