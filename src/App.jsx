f08b92c02c0d0d555849c5cf06a4714c96f929fd
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-06-29 12:21:52 -0400

    imported components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router ,Route} from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Videos from './Components/Videos';
+import Blog from './Components/Blog';
+import Merchandise from './Components/Merchandise';
+import Training from './Components/Training';
+import Subscribe from './Components/Subscribe';
+import Footer from './Components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+        <Router>
+          <div className="App">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={About} />
+            <Route exact path="/" component={Videos} />
+            <Route exact path="/" component={Blog} />
+            <Route exact path="/" component={Merchandise} />
+            <Route exact path="/" component={Training} />
+            <Route exact path="/" component={Subscribe} />
+            <Route exact path="/" component={Footer} />
+          </div>
+        </Router>
     );
   }
 }
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ class About extends Component {
     render() {
         return (
             <div className="About">
-
+                About
             </div>
         );
     }
