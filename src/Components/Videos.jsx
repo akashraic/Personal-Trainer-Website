3d8c5882d4405b535b7483a5fb1ebeb6877375fd
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-06-30 14:03:17 -0400

    Created structure for videos.jsx

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import { Link } from 'react-router-dom';
-import  {Grid, Row, Col, Image, Nav, NavItem} from 'react-bootstrap';
+import  {Grid, Row, Col} from 'react-bootstrap';
 import './Videos.css';
 
 class Videos extends Component {
@@ -10,8 +10,39 @@ class Videos extends Component {
             <div className="Videos">
                 <h1>Videos</h1>
                 <Grid>
-                    <Row>
-
+                    <Row className="Description-wrapper">
+                        <Col md={3} />
+                        <Col className="Description" md={6}>
+                            <p>This is the video section where you can find some videos from my
+                            upcoming video series</p>
+                        </Col>
+                        <Col md={3} />
+                    </Row>
+                    <Row className="Video-wrapper">
+                        <Col md={4} className="Video_1">
+                            <ReactPlayer
+                                className="player"
+                                url="assests/intro.mp4"
+                                width="100%"
+                                height="100%"
+                                controls />
+                        </Col>
+                        <Col md={4}>
+                            <ReactPlayer
+                                className="player"
+                                url="assests/intro.mp4"
+                                width="100%"
+                                height="100%"
+                                controls />
+                        </Col>
+                        <Col md={4}>
+                            <ReactPlayer
+                                className="player"
+                                url="assests/intro.mp4"
+                                width="100%"
+                                height="100%"
+                                controls />
+                        </Col>
                     </Row>
                 </Grid>
             </div>
