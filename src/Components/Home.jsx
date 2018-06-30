b839d890279d0e6f66c2828d422b6e66715750e7
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-06-29 21:55:40 -0400

    styles heading and logo and added menu items

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import  {Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import  {Grid, Row, Col, Image, Button} from 'react-bootstrap';
 import './Home.css';
 
 class Home extends Component {
     render() {
         return (
             <Grid>
-                <Row className="show-grid tet-center">
-                    <Col xs={4}>
-                        <h1>Holistic</h1>
+                <Row className="show-grid text-center Heading">
+                    <Col xs={3} />
+                    <Col className="Head-wrap-1" xs={2}>
+                        <h1 className="Head-1">Holistic</h1>
                     </Col>
-                    <Col className="Logo-wrapper" xs={6}>
+                    <Col className="Logo-wrapper" xs={2}>
                         <Image src="assests/Main_Logo.png" circle className="logo"/>
                     </Col>
-                    <Col xs={2}>
-                        <h1>Bodies</h1>
+                    <Col className="Head-wrap-2" xs={2}>
+                        <h1 className="Head-2">Bodies</h1>
+                    </Col>
+                    <Col xs={3} />
+                </Row>
+                <Row className="show-grid text-center Menu">
+                    <Col xs={2} />
+                    <Col className="Items">
+                        <h6>About</h6>
+                        <h6>Videos</h6>
+                        <h6>Blog</h6>
+                        <h6>Merchandise</h6>
+                        <h6>Training</h6>
+                        <h6>Subscribe</h6>
                     </Col>
                 </Row>
+                <hr />
             </Grid>
         );
     }
