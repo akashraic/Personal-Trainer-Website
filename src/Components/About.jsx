0d6f267158891068beb8c22deb7da8795d225492
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-06-30 12:00:12 -0400

    added Hussain picture

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,9 +9,32 @@ class About extends Component {
                 <Grid>
                     <h1>About</h1>
                     <Row className="about-us">
-                        <Col>
-                            
+                        <h2>About Us</h2>
+                        <Col xs={1} />
+                        <Col xs={5} className="triangle-wrapper">
+                            <Image src="assests/Triangle.png" responsive className="triangle"/>
                         </Col>
+                        <Col xs={5} className="About-us-wrapper">
+                            <p>Holistic bodies is about creating an holistic self. Holistic way of living involves the commitment to growth. Growth requires you to understand your own individual self, keeping an audit of your strength and weaknesses, and seizing the opportunities provided to you.
+                                Holistics bodies is a space where you can learn about yourself, use tools to improve yourself, and share your improvements with the rest of the community.
+                                We believe in empowering individuals through helping them explore their ethos, pathos and logos.
+                            </p>
+                        </Col>
+                        <Col xs={1} />
+                    </Row>
+                    <Row className="about-me">
+                        <h2>About Us</h2>
+                        <Col xs={1} />
+                        <Col xs={5} className="triangle-wrapper">
+                            <Image src="assests/Triangle.png" responsive className="triangle"/>
+                        </Col>
+                        <Col xs={5} className="About-us-wrapper">
+                            <p>Holistic bodies is about creating an holistic self. Holistic way of living involves the commitment to growth. Growth requires you to understand your own individual self, keeping an audit of your strength and weaknesses, and seizing the opportunities provided to you.
+                                Holistics bodies is a space where you can learn about yourself, use tools to improve yourself, and share your improvements with the rest of the community.
+                                We believe in empowering individuals through helping them explore their ethos, pathos and logos.
+                            </p>
+                        </Col>
+                        <Col xs={1} />
                     </Row>
                 </Grid>
             </div>
