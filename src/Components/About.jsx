e5c58452e93daae6e02c70c12f08e9cde75829a8
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-06-30 13:23:28 -0400

    completed structure for about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -25,14 +25,12 @@ class About extends Component {
                     <Row className="about-me">
                         <h2>About Us</h2>
                         <Col xs={1} />
-                        <Col xs={5} className="triangle-wrapper">
-                            <Image src="assests/Triangle.png" responsive className="triangle"/>
+                        <Col xs={5} className="person-wrapper">
+                            <Image src="assests/Hussain.jpg" responsive className="person"/>
                         </Col>
-                        <Col xs={5} className="About-us-wrapper">
-                            <p>Holistic bodies is about creating an holistic self. Holistic way of living involves the commitment to growth. Growth requires you to understand your own individual self, keeping an audit of your strength and weaknesses, and seizing the opportunities provided to you.
-                                Holistics bodies is a space where you can learn about yourself, use tools to improve yourself, and share your improvements with the rest of the community.
-                                We believe in empowering individuals through helping them explore their ethos, pathos and logos.
-                            </p>
+                        <Col xs={5} className="About-me-wrapper">
+                            <p>My name is Hussain Bokhari. My wealth of experience comes from the years of learning and applying growth strategies, creating opportunities for people around me to live their version of purposeful life.</p>
+                            <p>I have graduated with Ryerson University with a Bachelors and Masters of Engineering (Aerospace). A lot of people ask me why I am not an engineering, its because there are more than one ways to help people, the best and closest way to my heart has been _….</p>
                         </Col>
                         <Col xs={1} />
                     </Row>
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+import { Link } from 'react-router-dom';
+import  {Grid, Row, Col, Image, Nav, NavItem} from 'react-bootstrap';
 import './Videos.css';
 
 class Videos extends Component {
     render() {
         return (
             <div className="Videos">
+                <h1>Videos</h1>
+                <Grid>
+                    <Row>
 
+                    </Row>
+                </Grid>
             </div>
         );
     }
