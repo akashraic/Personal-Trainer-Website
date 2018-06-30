3dd8e6ef561960eecc1eaa04e0ab8fdf4e5ff805
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-06-30 14:55:31 -0400

    Created Structure for Merchandise.jsx

--- src/Components/Merchandise.jsx
+++ src/Components/Merchandise.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import  {Grid, Row, Col, Button} from 'react-bootstrap';
 import './Merchandise.css';
 
 class Merchandise extends Component {
     render() {
         return (
             <div className="Merchandise">
+                <h1>Merchandise</h1>
+                <Grid>
+                    <Row className="Description-wrapper">
+                        <Col md={4} />
+                        <Col className="Description" md={4}>
+                            <p>Click on the button below to view some of my Merchandise</p>
+                        </Col>
+                        <Col md={4} />
+                    </Row>
+                    <Row className="Button-wrapper-wrap">
+                        <Col md={3}/>
+                        <Col md={6} className="Button-wrapper">
+                            <Button block>
+                                Continue to Store
+                            </Button>
+                        </Col>
+                        <Col md={3}/>
+                    </Row>
+                </Grid>
 
             </div>
         );
