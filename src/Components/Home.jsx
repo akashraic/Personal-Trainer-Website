234c0c6b2fdca2f92a5abe07e718295ea2ae23fd
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-07-02 01:15:51 -0400

    added CSS for Home.jsx and made Home.jsx responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,33 +12,33 @@ class Home extends Component {
         return (
             <Grid>
                 <Row className="show-grid text-center Heading">
-                    <Col xs={3} />
-                    <Col className="Head-wrap-1" xs={2}>
+                    <Col md={3} xs={0} />
+                    <Col className="Head-wrap-1" md={2} xs={4}>
                         <h1 className="Head-1">Holistic</h1>
                     </Col>
-                    <Col className="Logo-wrapper" xs={2}>
+                    <Col className="Logo-wrapper" md={2} xs={4}>
                         <Image src="assests/Main_Logo.png" circle className="logo"/>
                     </Col>
-                    <Col className="Head-wrap-2" xs={2}>
+                    <Col className="Head-wrap-2" md={2} xs={4}>
                         <h1 className="Head-2">Bodies</h1>
                     </Col>
-                    <Col xs={3} />
+                    <Col md={3} xs={0}/>
                 </Row>
                 <Row className="show-grid text-center Menu-row">
-                    <Col xs={3} />
-                    <Col xs={6} className="Menu-items">
-                        <Nav bsStyle="pills" justified className="Items">
-                            <NavItem eventKey={1} className="item_6"onClick={goToAnchor('Home')}>Home</NavItem>
+                    <Col md={2} />
+                    <Col md={8} className="Menu-items">
+                        <Nav bsStyle="pills" justified className="Items" >
+                            <NavItem eventKey={1} className="item_6" onClick={goToAnchor('Home')}>Home</NavItem>
                             <NavItem eventKey={2} className="item_1" onClick={goToAnchor('About')}>About</NavItem>
                             <NavItem eventKey={3} className="item_2" onClick={goToAnchor('Videos')}>Videos</NavItem>
                             <NavItem eventKey={4} className="item_3" onClick={goToAnchor('Blog')}>Blog</NavItem>
                             <NavItem eventKey={5} className="item_4" onClick={goToAnchor('Merchandise')}>Merchandise</NavItem>
-                            <NavItem eventKey={6} className="item_5"onClick={goToAnchor('Training')}>Training</NavItem>
-                            <NavItem eventKey={7} className="item_6"onClick={goToAnchor('Subscribe')}>Subscribe</NavItem>
-                            <NavItem eventKey={8} className="item_6"onClick={goToAnchor('Contact')}>Contact</NavItem>
+                            <NavItem eventKey={6} className="item_5" onClick={goToAnchor('Training')}>Training</NavItem>
+                            <NavItem eventKey={7} className="item_6" onClick={goToAnchor('Subscribe')}>Subscribe</NavItem>
+                            <NavItem eventKey={8} className="item_6" onClick={goToAnchor('Contact')}>Contact</NavItem>
 
                         </Nav>
-                        <Col xs={3} />
+                        <Col md={2} />
                     </Col>
                 </Row>
                 <hr />
