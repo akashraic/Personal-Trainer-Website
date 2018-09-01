b3b12bf9462c495db7badae52693478269702fd1
Akash Chhabria <akash.chhabria@ryerson.ca>
2018-09-01 19:04:07 -0400

    added more titles and divs to the first title

--- src/Components/Training.jsx
+++ src/Components/Training.jsx
@@ -28,23 +28,54 @@ class Training extends Component {
 
                                 <Panel eventKey="1">
                                     <Panel.Heading>
-                                        <Panel.Title toggle>Collapsible Group Item #1</Panel.Title>
+                                        <Panel.Title toggle>Session Packs</Panel.Title>
                                     </Panel.Heading>
                                     <Panel.Body collapsible>
-                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-                                        dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-                                        moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-                                        assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                                        wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-                                        butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                                        aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                        labore sustainable VHS.
+                                        <div className="columns">
+                                            <ul className="price">
+                                                <li className="header">Basic</li>
+                                                <li className="grey">$ 9.99 / year</li>
+                                                <li>10GB Storage</li>
+                                                <li>10 Emails</li>
+                                                <li>10 Domains</li>
+                                                <li>1GB Bandwidth</li>
+                                            </ul>
+                                        </div>
+                                        <div className="columns">
+                                            <ul className="price">
+                                                <li className="header">Basic</li>
+                                                <li className="grey">$ 9.99 / year</li>
+                                                <li>10GB Storage</li>
+                                                <li>10 Emails</li>
+                                                <li>10 Domains</li>
+                                                <li>1GB Bandwidth</li>
+                                            </ul>
+                                        </div>
+                                        <div className="columns">
+                                            <ul className="price">
+                                                <li className="header">Basic</li>
+                                                <li className="grey">$ 9.99 / year</li>
+                                                <li>10GB Storage</li>
+                                                <li>10 Emails</li>
+                                                <li>10 Domains</li>
+                                                <li>1GB Bandwidth</li>
+                                            </ul>
+                                        </div>
+                                        <div className="columns">
+                                            <ul className="price">
+                                                <li className="header">Basic</li>
+                                                <li className="grey">$ 9.99 / year</li>
+                                                <li>10GB Storage</li>
+                                                <li>10 Emails</li>
+                                                <li>10 Domains</li>
+                                                <li>1GB Bandwidth</li>
+                                            </ul>
+                                        </div>
                                     </Panel.Body>
                                 </Panel>
                                 <Panel eventKey="2">
                                     <Panel.Heading>
-                                        <Panel.Title toggle>Collapsible Group Item #2</Panel.Title>
+                                        <Panel.Title toggle>Sports Specific Private Sessions</Panel.Title>
                                     </Panel.Heading>
                                     <Panel.Body collapsible>
                                         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
@@ -60,7 +91,39 @@ class Training extends Component {
                                 </Panel>
                                 <Panel eventKey="3">
                                     <Panel.Heading>
-                                        <Panel.Title toggle>Collapsible Group Item #3</Panel.Title>
+                                        <Panel.Title toggle>Nutrition Guidance & Planning</Panel.Title>
+                                    </Panel.Heading>
+                                    <Panel.Body collapsible>
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
+                                        dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
+                                        moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                                        assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+                                        wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                        butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+                                        aesthetic synth nesciunt you probably haven't heard of them accusamus
+                                        labore sustainable VHS.
+                                    </Panel.Body>
+                                </Panel>
+                                <Panel eventKey="4">
+                                    <Panel.Heading>
+                                        <Panel.Title toggle>Sample Session Training (1-4 Weeks Samples Available)</Panel.Title>
+                                    </Panel.Heading>
+                                    <Panel.Body collapsible>
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
+                                        dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
+                                        moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                                        assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+                                        wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                        butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+                                        aesthetic synth nesciunt you probably haven't heard of them accusamus
+                                        labore sustainable VHS.
+                                    </Panel.Body>
+                                </Panel>
+                                <Panel eventKey="5">
+                                    <Panel.Heading>
+                                        <Panel.Title toggle>Subscriptions</Panel.Title>
                                     </Panel.Heading>
                                     <Panel.Body collapsible>
                                         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
